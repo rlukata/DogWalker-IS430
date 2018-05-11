e2f71d7f6fd33e211fc48fbe45f8739448a4f08f
Maencina <38990523+Maencina@users.noreply.github.com>
2018-05-10 18:38:08 -0700

    Fixed e-mail address validation regex

--- elements/layout.php
+++ elements/layout.php
@@ -114,7 +114,7 @@
                     <input class="signin" type="text" placeholder="Enter your Phone Number" name="PhoneNumber" maxlength="14" pattern="(?:(?:(\s*\(?([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\)?\s*(?:[.-]\s*)?)([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})" title="Please enter a valid phone number: (425)555-1212" required>
 					
                     <label for="email"><b>Email</b></label>
-                    <input class="signin" type="text" placeholder="Enter Email" name="email" pattern="\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b." title="Enter a valid e-mail address: JohnDoe@gmail.com" required>
+                    <input class="signin" type="text" placeholder="Enter Email" name="email" pattern="\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}\b." title="Enter a valid e-mail address: JohnDoe@gmail.com" required>
 
                     <label for="psw"><b>Password</b></label>
                     <input class="signin" type="password" placeholder="Enter Password" name="psw" required>
