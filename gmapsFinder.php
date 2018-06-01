93c173af0f85ac29786dfffcf607fe0517bacd19
Maencina <38990523+Maencina@users.noreply.github.com>
2018-05-31 17:19:37 -0700

    Update gmapsFinder.php

--- gmapsFinder.php
+++ gmapsFinder.php
@@ -1,14 +1,17 @@
 <!DOCTYPE html>
-<?php include 'included.php'; ?>
+<?php 
+    include 'included.php';
+    include 'session.php';
+?>
 <html>
     <!-- Head -->
     <head>
-            <title>3Musqueteers</title>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
-            <style>
-                <?php include 'css/main.css';?>
-            </style>
+        <title>3Musqueteers</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+        <style>
+            <?php include 'css/main.css';?>
+        </style>
     </head>
 
     <!-- Body -->
@@ -23,13 +26,16 @@
 
             <!-- Side content -->
             <div class="side">
-                <?php require 'elements/signinButton.html'; ?>
-                <?php require 'elements/loginButton.html'; ?>
+                <b id="logout"><a href="logout.php">Log Out</a></b>
+				<br><br><br>
+				<img src="images/Dog008.jpg" alt="HTML5 Icon" style="width:350px;height:265px;">
+				<br>
             </div>
 
             <!-- Main content -->
             <div class="main">
-               
+                <?php RetrieveUserNameFromDB($login_session); ?>
+				<b>Dog Walkers available near you: (Map may take a minute to load)</b><br>
                 <iframe
                   width="600"
                   height="450"
@@ -40,6 +46,42 @@
         </div>
 
         <?php require 'elements/footer.html'; ?>
+		
+		<?php
+			function RetrieveUserNameFromDB($userEmail)
+                {
+                        // Create connection
+                        $conn = new mysqli('localhost', 'root', '', 'customers');
+                        // Check connection
+                        if ($conn->connect_error)
+                        {
+                                die("Connection failed: " . $conn->connect_error);
+                        } 
 
+                        // Useful for troubleshooting
+                        // echo "<b>Connection to MySQL DB established!</b> <br>";
+
+                        $sql = "SELECT FName,LName FROM Dog_Owners WHERE email='$userEmail'";
+                        // Useful for troubleshooting
+                        // echo "SQL Statemet: $sql <br>";
+						
+						$result = $conn->query($sql);
+						if ($result->num_rows > 0)
+						{
+							$row = $result->fetch_assoc();
+							echo "<h2>Welcome: " .$row['FName'] ." " .$row['LName'] ."</h2><br>";
+						}
+						else
+						{
+							die("Connection failed: " . $conn->connect_error);
+						}
+
+                        $conn->close();	
+                }
+		
+		?>
+
+		
+		
     </body>
 </html>
