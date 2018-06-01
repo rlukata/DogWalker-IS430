0d1f81f832b9feedec08eab78bcd7fb8e12e372d
Maencina <38990523+Maencina@users.noreply.github.com>
2018-05-31 17:13:50 -0700

    Update about.php

--- about.php
+++ about.php
@@ -6,6 +6,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <style><?php include 'css/main.css';?></style>
+
     </head>
 
     <!-- Body -->
@@ -20,26 +21,32 @@
 
             <!-- Side content -->
             <div class="side">
-                <?php require 'elements/signinButton.html'; ?>
-                <?php require 'elements/loginButton.html'; ?>
+				<br><br>
+					<img src="images/Walkingdogs_001.jpg" alt="HTML5 Icon" style="width:390px;height:300px;" >
+				<br>
             </div>
 
             <!-- Main content -->
             <div class="main">
-                    <p>We are the 3Musqueteers!</p>
-                    <p>We are passionate about your dogs and their health...</p>
-                    <p>Some of the benefits of walking for your pet are: </p>
-                    <p>1. It helps with your pet's weight control.</p>
-                    <p>2. helps your pet's digestive system.</p>
-                    <p>3. It stops their destructive behavior and hyperactivity.</p>
-                    <p>Here's what we do: </p>
-                    <p>We pair available dog walkers with people who
-                                               have the need for their dog(s) to be walked.</p>
-                    <p>Here's why we do it: </p>
-                    <p>We do this for the love of healthy and happy dogs!</p>
-                    <p>Here's who we are: </p>
-                    <p>We are three men looking to assist our community
-                       by providing a free and safe service for pet owners.</p>
+                    <p><b>We are the 3Musqueteers!</b></p>
+                    <p>...and, we are passionate about our canine friends.</p>
+					<p> It is no secret that our canine companions enrich our lives. </p>
+					<p> And, in order to prolong our best friend's lives we need to keep them healty. </p>
+					<hr>
+					
+                    <p><b>What are some of the benefits of waking your dog?</b></p>
+					<ol>
+						<li>Keeps their heart and lungs in top shape</li>
+						<li>It keeps their joints healthy</li>
+						<li>It helps their digestive and circulatory system</li>
+						<li>It stops their destructive behavior and hyperactivity</li>
+					</ol>
+					<hr>
+                    
+                    <p><b>How does it work?</b></p>
+                    <p>Simple: we pair our highly trained dog walkers with people who
+                       have the need for their dog(s) to be walked.</p>
+                    
             </div>
         </div>
 
