355f5f61e6d8b6026875ffe92cf71918d8129641
Maencina <38990523+Maencina@users.noreply.github.com>
2018-05-31 17:11:38 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,45 +1,56 @@
 <?php include 'included.php'; ?>
 <html>
+    <!-- Head -->
+    <head>
+        <title>3Musqueteers</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <style><?php include 'css/main.css';?></style>
 
-	<!-- Head -->
-	<head>
-		<title>3Musqueteers</title>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<style><?php include 'css/main.css';?></style>
-	</head>
+    </head>
 
-        
-	<!-- Body -->
-	<body>
-		
-		<?php headWeb() ?>
-		
-		<?php navBar("home"); ?>
-			
-		<!-- The content -->
-		<div class="row">
-			
-                        <!-- Side content -->
-                        <div class="side">
-                            <?php signinButton() ?>
-                            <?php loginButton() ?>
-							<br><b>OR</b></br>
-							<?php signinAsDogWalkerButton() ?>
-                        </div>
-			
-			<!-- Main content -->
-			<div class="main">
-				<p>My dog walker understands that my dogs are my world. She was in constant contact sending me updates 
-				throughout the day. She took care of my dogs are as if they were her own!</p>
-				<p><i>M. Silverstone. Seattle, WA</i></p>
-				</br>
-				<p>John S. is an amazing dogwalker. He takes my labradoodle (Diesel) for runs and my dog loves it!</p>
-				</p><i>P. Gomez. Portland, OR</i></p>
-			</div>
-		</div>
-		
-		<?php footer(); ?>
-		
-	</body>
+    <!-- Body -->
+    <body>
+
+        <?php require 'elements/header.html' ?>
+
+        <?php navBar("about"); ?>
+
+        <!-- The content -->
+        <div class="row">
+
+            <!-- Side content -->
+            <div class="side">
+				<br><br>
+					<img src="images/Walkingdogs_001.jpg" alt="HTML5 Icon" style="width:390px;height:300px;" >
+				<br>
+            </div>
+
+            <!-- Main content -->
+            <div class="main">
+                    <p><b>We are the 3Musqueteers!</b></p>
+                    <p>...and, we are passionate about our canine friends.</p>
+					<p> It is no secret that our canine companions enrich our lives. </p>
+					<p> And, in order to prolong our best friend's lives we need to keep them healty. </p>
+					<hr>
+					
+                    <p><b>What are some of the benefits of waking your dog?</b></p>
+					<ol>
+						<li>Keeps their heart and lungs in top shape</li>
+						<li>It keeps their joints healthy</li>
+						<li>It helps their digestive and circulatory system</li>
+						<li>It stops their destructive behavior and hyperactivity</li>
+					</ol>
+					<hr>
+                    
+                    <p><b>How does it work?</b></p>
+                    <p>Simple: we pair our highly trained dog walkers with people who
+                       have the need for their dog(s) to be walked.</p>
+                    
+            </div>
+        </div>
+
+        <?php require 'elements/footer.html'; ?>
+
+    </body>
 </html>
