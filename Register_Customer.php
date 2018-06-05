87cac4e8858643eb9dcc0e2406319cca1ff2ea25
Rami Lukata <rlukata@cityuniversity.edu>
2018-06-04 23:26:27 -0700

    Merge origin/master
    Conflicts:
    	Register_Customer.php
    	Register_DogWalker.php
    	login.php

--- Register_Customer.php
+++ Register_Customer.php
@@ -1,6 +1,6 @@
+<?php include 'included.php'; ?>
 <!DOCTYPE html>
 <html>
-<<<<<<< HEAD
     <!-- Head -->
     <head>
         <title>3Musqueteers</title>
@@ -56,191 +56,90 @@
                     }
 				
 				
-                /* Good for troubleshooting ****************
-                echo "First Name: $firstName<br>";
-                echo "Last Name: $lastName<br>";
-                echo "Zip Code: $zipCode<br>";
-                echo "E-mail: $userEmail<br>";
-                echo "Phone Number: $phoneNumber<br>";
-                echo "Your Password: $userPassword<br>";
-                echo "Your 2nd Password: $user2Password<br>";
-                ********************************************/
-                ValidateUserPassword($userPassword, $user2Password);
-                CreateMySQLUser($firstName, $lastName, $zipCode, $userEmail, $phoneNumber, $userPassword);
-                echo "</br></br>";
-                
-				
-                /* Functions go here */
-                // Validate that both passwords match
-                function ValidateUserPassword($userPassword, $user2Password)
-                {
-                    if ($userPassword !== $user2Password)
+                    /* Good for troubleshooting ****************
+                    echo "First Name: $firstName<br>";
+                    echo "Last Name: $lastName<br>";
+                    echo "Zip Code: $zipCode<br>";
+                    echo "E-mail: $userEmail<br>";
+                    echo "Phone Number: $phoneNumber<br>";
+                    echo "Your Password: $userPassword<br>";
+                    echo "Your 2nd Password: $user2Password<br>";
+                    ********************************************/
+                    ValidateUserPassword($userPassword, $user2Password);
+                    CreateMySQLUser($firstName, $lastName, $zipCode, $userEmail, $phoneNumber, $userPassword);
+                    echo "</br></br>";
+
+
+                    /* Functions go here */
+                    // Validate that both passwords match
+                    function ValidateUserPassword($userPassword, $user2Password)
                     {
-                        echo "<h1>Ooops, passwords do not match! Please try again! </h1><br>";
-                        echo '<a href="index.php">Go back to the main page</a>';
-                        exit;
+                        if ($userPassword !== $user2Password)
+                        {
+                            echo "<h1>Ooops, passwords do not match! Please try again! </h1><br>";
+                            echo '<a href="index.php">Go back to the main page</a>';
+                            exit;
+                        }
+                    }	
+
+                    /*****************************************************
+                    * This function will sanitize user input
+                    * Specifically fields like first and last name
+                    * Even though these fields are restriced in size
+                    * users can still enter dangerous text.
+                    *****************************************************/
+                    function SanitizeUserInput($input)
+                    {
+                        // Dangerous words not allowed
+                        $wordsNotAllowed = array("/delete/i", "/update/i","/union/i","/insert/i","/drop/i","/evil/i","/--/i");
+                        // Remove dangerous words from first name
+                        $input = preg_replace($wordsNotAllowed , "", $input);
+                        $input = escapeshellarg($input);
+                        // strip tags
+                        $input = strip_tags($input);
+                        // strip slashes
+                        $input = stripslashes($input);
+                        return $input;
                     }
-                }	
-				
-                /*****************************************************
-                * This function will sanitize user input
-                * Specifically fields like first and last name
-                * Even though these fields are restriced in size
-                * users can still enter dangerous text.
-                *****************************************************/
-                function SanitizeUserInput($input)
-                {
-                    // Dangerous words not allowed
-                    $wordsNotAllowed = array("/delete/i", "/update/i","/union/i","/insert/i","/drop/i","/evil/i","/--/i");
-                    // Remove dangerous words from first name
-                    $input = preg_replace($wordsNotAllowed , "", $input);
-                    $input = escapeshellarg($input);
-                    // strip tags
-                    $input = strip_tags($input);
-                    // strip slashes
-                    $input = stripslashes($input);
-                    return $input;
-                }
-				
-                function CreateMySQLUser($firstName, $lastName, $zipCode, $userEmail, $phoneNumber, $userPassword)
-                {
-                    // Useful for troubleshooting
-                    // echo "<b>Creating User: <i>$firstName $lastName</i></b><br>";
-                    // Create connection
-                    $conn = new mysqli('localhost', 'root', '', 'customers');
-                    // Check connection
-                    if ($conn->connect_error)
+
+                    function CreateMySQLUser($firstName, $lastName, $zipCode, $userEmail, $phoneNumber, $userPassword)
                     {
-                        die("Connection failed: " . $conn->connect_error);
-                    } 
-                    // Useful for troubleshooting
-                    // echo "<b>Connection to MySQL DB established!</b> <br>";
-                    $stmt = $conn->prepare("INSERT INTO dog_owners (FName, LName, ZipCode, Email, PhoneNumber, Password) VALUES (?, ?, ?, ?, ?, MD5(?))");
-                    $stmt->bind_param('ssisis', $firstName, $lastName, $zipCode, $userEmail, $phoneNumber, $userPassword);
-                    $stmt->execute();
-                    $stmt->store_result();
-                    // Useful for troubleshooting
-                    // echo "SQL Statemet: $sql <br>";
-                    if ($stmt->num_rows == 0)
-                    {
-                        echo "<h1>Welcome $firstName $lastName!!!</h1><br>";
-                        echo "<b>Thank you for registering with our service. You are now able to hire a dog walker in your area.</b><br>";
+                        // Useful for troubleshooting
+                        // echo "<b>Creating User: <i>$firstName $lastName</i></b><br>";
+                        // Create connection
+                        $conn = new mysqli('localhost', 'root', '', 'customers');
+                        // Check connection
+                        if ($conn->connect_error)
+                        {
+                            die("Connection failed: " . $conn->connect_error);
+                        } 
+                        // Useful for troubleshooting
+                        // echo "<b>Connection to MySQL DB established!</b> <br>";
+                        $stmt = $conn->prepare("INSERT INTO dog_owners (FName, LName, ZipCode, Email, PhoneNumber, Password) VALUES (?, ?, ?, ?, ?, MD5(?))");
+                        $stmt->bind_param('ssisis', $firstName, $lastName, $zipCode, $userEmail, $phoneNumber, $userPassword);
+                        $stmt->execute();
+                        $stmt->store_result();
+                        // Useful for troubleshooting
+                        // echo "SQL Statemet: $sql <br>";
+                        if ($stmt->num_rows == 0)
+                        {
+                            echo "<h1>Welcome $firstName $lastName!!!</h1><br>";
+                            echo "<b>Thank you for registering with our service. You are now able to hire a dog walker in your area.</b><br>";
+                        }
+                        else
+                        {
+                            // Good for troubleshooting, but not a good idea to show this information to user
+                            // echo "Error: " . $sql . "<br>" . $conn->error;
+                            // User could be entering SQL cmdlets with quotes
+                            // Display a generic error message instead
+                            echo "<b>Error: Unable to register user: $firstName, $lastName. Please verify your information and try again!</b><br>";
+                            echo "<b>If you are unable to register, please contact us!</b>";				
+                        }
+                        $conn->close();	
                     }
-                    else
-                    {
-                        // Good for troubleshooting, but not a good idea to show this information to user
-                        // echo "Error: " . $sql . "<br>" . $conn->error;
-                        // User could be entering SQL cmdlets with quotes
-                        // Display a generic error message instead
-                        echo "<b>Error: Unable to register user: $firstName, $lastName. Please verify your information and try again!</b><br>";
-                        echo "<b>If you are unable to register, please contact us!</b>";				
-                    }
-                    $conn->close();	
-                }
                 ?>
             </div>
-=======
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>3 Musketeers' Dog Walking Service</title>
-
-<link rel="stylesheet" href="CascadeStyle.css">
-
- <div class="container-fluid">
-    <div class="container">
-        <div class="row text-center">
-            <img class="img-responsive center-block" style="float:right; width:300px; height:150px;">
->>>>>>> origin/master
         </div>
-        
-     </div>
- </div>
-</head>
-
-<body>
-
-<div id="container">
-	<div id="header">
-		<h1> Welcome to our dog walking service </h1>
-		<h3> (Happy Clients - Happy Dogs!) </h3>
-	</div>
-	<div id="body">
-	
-<?php
-// TODO: We should use htmlspecialchars($_SERVER["PHP_SELF"]) so the user gets the error messages on the same page!!!
-// TODO: Need to use isset to verify all fields *****
-$firstName = $_POST["FName"];
-$lastName = $_POST["LName"];
-$zipCode = $_POST["ZipCode"];
-$userEmail = $_POST["email"];
-$phoneNumber = $_POST["PhoneNumber"];
-$userPassword = $_POST["psw"];
-$user2Password = $_POST["psw-repeat"];
-
-
-echo "First Name: $firstName<br>";
-echo "Last Name: $lastName<br>";
-echo "Zip Code: $zipCode<br>";
-echo "E-mail: $userEmail<br>";
-echo "Phone Number: $phoneNumber<br>";
-echo "Your Password: $userPassword<br>";
-echo "Your 2nd Password: $user2Password<br>";
-
-ValidateUserPassword($userPassword, $user2Password);
-CreateMySQLUser($firstName, $lastName, $zipCode, $userEmail, $phoneNumber, $userPassword);
-echo "</br></br>";
-
-
-/* Functions go here */
-function ValidateUserPassword($userPassword, $user2Password)
-{
-	if ($userPassword !== $user2Password)
-	{
-		echo "Ooops, passwords do not match! Please try again! <br>";
-		exit;
-	}
-}
-
-
-function CreateMySQLUser($firstName, $lastName, $zipCode, $userEmail, $phoneNumber, $userPassword)
-{
-	echo "<b>Creating User: <i>$firstName $lastName</i></b><br>";
-	// Create connection
-	$conn = new mysqli('localhost', 'root', '', 'customers');
-	// Check connection
-	if ($conn->connect_error)
-	{
-		die("Connection failed: " . $conn->connect_error);
-	} 
-	echo "<b>Connection to MySQL DB established!</b> <br>";
-	$sql = "INSERT INTO users (FName, LName, ZipCode, Email, PhoneNumber, Password)
-	VALUES ('$firstName' , '$lastName', '$zipCode', '$userEmail', '$phoneNumber', '$userPassword')";
-
-	echo "SQL Statemet: $sql <br>";
-	if ($conn->query($sql) === TRUE)
-	{
-		echo "<b>New record created successfully</b><br>";
-	}
-	else
-	{
-		echo "Error: " . $sql . "<br>" . $conn->error;
-	}
-
-	$conn->close();	
-}
-
-
-?>
-</br></br>
-<a href="index.php">Go back to the main page</a>
-	</div>
-	<div id="footer">
-		<hr>
-		<h2 class="left" id = "contact">Webpage Created by</h2>
-		<small id = "name"><i>Mauricio A. Encina <br /><a href="mailto:mencina@cityu.edu">contact me</a><br />Copyright &copy; 2018</i></small>
-	</div>
-</div>
-
-</body>
-
+        <?php require 'elements/footer.html'; ?>
+    </body>
 </html> 
--- Register_DogWalker.php
+++ Register_DogWalker.php
@@ -19,7 +19,6 @@
             <!-- Main content -->
             <div class="main">
                 <?php
-<<<<<<< HEAD
                     if ($_SERVER["REQUEST_METHOD"] == "POST")
                     {
                         if(isset($_POST["FName"])){
@@ -55,114 +54,89 @@
                             $user2Password = $_POST["psw-repeat"];
                         }
                     }
-				
-=======
-                // TODO: Sanitize FName and LName
-                // TODO: Need to use isset to verify all fields are set as follows:
-                /* The following is an example of using isset
-                if ($_SERVER["REQUEST_METHOD"] == "POST")
-                { 
-                  if (isset($_POST['FName']))
-                  {
-                    $firstName = $_POST["FName"];
-                  }
-                */
-
-                $firstName = $_POST["FName"];
-                $lastName = $_POST["LName"];
-                $zipCode = $_POST["ZipCode"];
-                $userEmail = $_POST["email"];
-                $phoneNumber = $_POST["PhoneNumber"];
-                $userPassword = $_POST["psw"];
-                $user2Password = $_POST["psw-repeat"];
-
-                isset($firstName, $lastName, $zipCode, $userEmail, $phoneNumber, $userPassword, $user2Password);
-
->>>>>>> origin/master
-                /* Good for troubleshooting ****************
-                echo "First Name: $firstName<br>";
-                echo "Last Name: $lastName<br>";
-                echo "Zip Code: $zipCode<br>";
-                echo "E-mail: $userEmail<br>";
-                echo "Phone Number: $phoneNumber<br>";
-                echo "Your Password: $userPassword<br>";
-                echo "Your 2nd Password: $user2Password<br>";
-                ********************************************/
-                ValidateUserPassword($userPassword, $user2Password);
-                CreateMySQLUser($firstName, $lastName, $zipCode, $userEmail, $phoneNumber, $userPassword);
-                echo "</br></br>";
-                /* Functions go here */
-                function ValidateUserPassword($userPassword, $user2Password)
-                {
-                    if ($userPassword !== $user2Password)
+                    /* Good for troubleshooting ****************
+                    echo "First Name: $firstName<br>";
+                    echo "Last Name: $lastName<br>";
+                    echo "Zip Code: $zipCode<br>";
+                    echo "E-mail: $userEmail<br>";
+                    echo "Phone Number: $phoneNumber<br>";
+                    echo "Your Password: $userPassword<br>";
+                    echo "Your 2nd Password: $user2Password<br>";
+                    ********************************************/
+                    ValidateUserPassword($userPassword, $user2Password);
+                    CreateMySQLUser($firstName, $lastName, $zipCode, $userEmail, $phoneNumber, $userPassword);
+                    echo "</br></br>";
+                    /* Functions go here */
+                    function ValidateUserPassword($userPassword, $user2Password)
                     {
-                        echo "<h1>Ooops, passwords do not match! Please try again! </h1><br>";
-                        echo '<a href="index.php">Go back to the main page</a>';
-                        exit;
+                        if ($userPassword !== $user2Password)
+                        {
+                            echo "<h1>Ooops, passwords do not match! Please try again! </h1><br>";
+                            echo '<a href="index.php">Go back to the main page</a>';
+                            exit;
+                        }
                     }
-                }
 
-                /*****************************************************
-                * This function will sanitize user input
-                * Specifically fields like first and last name
-                * Even though these fields are restriced in size
-                * users can still enter dangerous text.
-                *****************************************************/
-                function SanitizeUserInput($input)
-                {
-                    // Dangerous words not allowed
-                    $wordsNotAllowed = array("/delete/i", "/update/i","/union/i","/insert/i","/drop/i","/evil/i","/--/i");
-                    // Remove dangerous words from first name
-                    $input = preg_replace($wordsNotAllowed , "", $input);
-                    $input = escapeshellarg($input);
-                    // strip tags
-                    $input = strip_tags($input);
-                    // strip slashes
-                    $input = stripslashes($input);
-                    return $input;
-                }
-				
-				
-                function CreateMySQLUser($firstName, $lastName, $zipCode, $userEmail, $phoneNumber, $userPassword)
-                {
-                    // Useful for troubleshooting
-                    // echo "<b>Creating User: <i>$firstName $lastName</i></b><br>";
-                    // Create connection
-                    $conn = new mysqli('localhost', 'root', '', 'customers');
-                    // Check connection
-                    if ($conn->connect_error)
+                    /*****************************************************
+                    * This function will sanitize user input
+                    * Specifically fields like first and last name
+                    * Even though these fields are restriced in size
+                    * users can still enter dangerous text.
+                    *****************************************************/
+                    function SanitizeUserInput($input)
                     {
-                        die("Connection failed: " . $conn->connect_error);
-                    } 
-                    // Useful for troubleshooting
-                    // echo "<b>Connection to MySQL DB established!</b> <br>";
-                    $stmt = $conn->prepare("INSERT INTO dog_walkers (FName, LName, ZipCode, Email, PhoneNumber, Password) VALUES (?, ?, ?, ?, ?, MD5(?))");
-                    $stmt->bind_param('ssisis', $firstName, $lastName, $zipCode, $userEmail, $phoneNumber, $userPassword);
-                    $stmt->execute();
-                    $stmt->store_result();
-                    // Useful for troubleshooting
-                    // echo "SQL Statemet: $sql <br>";
-                    if ($stmt->num_rows == 0)
-                    {
-                        echo "<h1>Welcome Dog Walker: $firstName $lastName!!!</h1><br>";
-                        echo "<b>We are looking forward to a long and fruitful relationship!</b><br>";
+                        // Dangerous words not allowed
+                        $wordsNotAllowed = array("/delete/i", "/update/i","/union/i","/insert/i","/drop/i","/evil/i","/--/i");
+                        // Remove dangerous words from first name
+                        $input = preg_replace($wordsNotAllowed , "", $input);
+                        $input = escapeshellarg($input);
+                        // strip tags
+                        $input = strip_tags($input);
+                        // strip slashes
+                        $input = stripslashes($input);
+                        return $input;
                     }
-                    else
-                    {
-                        // Good for troubleshooting, but not a good idea to show this information to user
-                        // echo "Error: " . $sql . "<br>" . $conn->error;
 
-                        // User could be entering SQL cmdlets with quotes
-                        // Display a generic error message instead
-                        echo "<b>Error: Unable to register user: $firstName, $lastName. Please verify your information and try again!</b><br>";
-                        echo "<b>If you are unable to register, please contact us!</b>";
+
+                    function CreateMySQLUser($firstName, $lastName, $zipCode, $userEmail, $phoneNumber, $userPassword)
+                    {
+                        // Useful for troubleshooting
+                        // echo "<b>Creating User: <i>$firstName $lastName</i></b><br>";
+                        // Create connection
+                        $conn = new mysqli('localhost', 'root', '', 'customers');
+                        // Check connection
+                        if ($conn->connect_error)
+                        {
+                            die("Connection failed: " . $conn->connect_error);
+                        } 
+                        // Useful for troubleshooting
+                        // echo "<b>Connection to MySQL DB established!</b> <br>";
+                        $stmt = $conn->prepare("INSERT INTO dog_walkers (FName, LName, ZipCode, Email, PhoneNumber, Password) VALUES (?, ?, ?, ?, ?, MD5(?))");
+                        $stmt->bind_param('ssisis', $firstName, $lastName, $zipCode, $userEmail, $phoneNumber, $userPassword);
+                        $stmt->execute();
+                        $stmt->store_result();
+                        // Useful for troubleshooting
+                        // echo "SQL Statemet: $sql <br>";
+                        if ($stmt->num_rows == 0)
+                        {
+                            echo "<h1>Welcome Dog Walker: $firstName $lastName!!!</h1><br>";
+                            echo "<b>We are looking forward to a long and fruitful relationship!</b><br>";
+                        }
+                        else
+                        {
+                            // Good for troubleshooting, but not a good idea to show this information to user
+                            // echo "Error: " . $sql . "<br>" . $conn->error;
+
+                            // User could be entering SQL cmdlets with quotes
+                            // Display a generic error message instead
+                            echo "<b>Error: Unable to register user: $firstName, $lastName. Please verify your information and try again!</b><br>";
+                            echo "<b>If you are unable to register, please contact us!</b>";
+                        }
+                        $conn->close();	
                     }
-                    $conn->close();	
-                }
-                ?>
+                    ?>
+                </div>
             </div>
-        </div>
         <?php require 'elements/footer.html'; ?>
-    
     </body>
 </html> 
